b5067d939d0a076d4fcbb464144ec5da056e3d61
HelenaP216 <paavilh@gmail.com>
2026-02-17 20:34:13 +0200

    describe what you changed

---
+++ Week1/Day1/ExerciseXP2.py
@@ -0,0 +1,32 @@
+
+# 🌟 Exercise 2: Cinemax #2
+
+# Key Python Topics:
+#    Looping through dictionaries
+#    Conditionals
+#    Calculations
+
+# Instructions
+
+# Write a program that calculates the total cost of 
+# movie tickets for a family based on their ages.
+
+#     Family members’ ages are stored in a dictionary.
+#    The ticket pricing rules are as follows:
+#        Under 3 years old: Free
+#        3 to 12 years old: $10
+#        Over 12 years old: $15
+
+# Family Data:
+
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+
+#    Loop through the family dictionary to calculate
+#        the total cost.
+#    Print the ticket price for each family member.
+#    Print the total cost at the end.
+
+
+# Bonus:
+# Allow the user to input family members’ names and
+#  ages, then calculate the total ticket cost.

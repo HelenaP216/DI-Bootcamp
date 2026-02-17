e06bb414e34cc4940531905b20887cac9d1cca86
HelenaP216 <paavilh@gmail.com>
2026-02-17 18:51:30 +0200

    describe what you changed

---
+++ Week1/Day1/CourseEx1.py
@@ -0,0 +1,14 @@
+
+# Working with the following string:
+
+description = "strings are..."
+
+#    1. make it all uper case
+#    2. replace the word "are" to "is"
+#´   3.  print just the word "strings"
+
+print("strings are... ".upper())
+print(description.upper())
+print(description.replace("are", "is"))
+print(description[0:7])
+

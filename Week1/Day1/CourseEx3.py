2320e51fdcda4f82ef1d33fcebeb93b13b2a5007
HelenaP216 <paavilh@gmail.com>
2026-02-17 19:43:20 +0200

    describe what you changed

---
+++ Week1/Day1/CourseEx3.py
@@ -0,0 +1,18 @@
+# Check what is the type of each value, then change 
+# it: if it is a string, make it an integer and 
+# vice-versa:
+
+bank_balance = '33000'
+phone_number = 532287514
+
+print(type(bank_balance))
+print(type(phone_number))
+
+bank_balance = int("33000")
+phone_number = str(532287514)
+
+print(type(bank_balance))
+print(type(phone_number))
+
+# print(bank_balance)
+# print(phone_number)
---
+++ Week1/Day1/CourseEx4.py
@@ -0,0 +1,9 @@
+# In the python shell, Create a variable called 
+# first_name and a variable called last_name, and 
+# then print your full name using those two 
+# variables
+
+first_name = "Helena"
+last_name = "Paavilainen"
+
+print(first_name + " " + last_name)

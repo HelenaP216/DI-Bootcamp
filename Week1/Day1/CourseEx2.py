c9019f142be620ad030d7ab2cfa143bc39164103
HelenaP216 <paavilh@gmail.com>
2026-02-17 19:01:57 +0200

    describe what you changed

---
+++ Week1/Day1/CourseEx2.py
@@ -0,0 +1,10 @@
+
+
+# In the python shell, Create a variable called 
+# my_age, use python to know how old you will be 
+# in 123879 years
+
+my_age = 36
+future_point = 123879
+my_age_in_future_point = my_age + future_point
+print(my_age_in_future_point)

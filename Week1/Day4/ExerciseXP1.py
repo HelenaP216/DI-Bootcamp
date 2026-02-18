1ae48ddaab60609fbf7c4436e8fd1c336c15b28d
HelenaP216 <paavilh@gmail.com>
2026-02-19 00:22:07 +0200

    describe what you changed

---
+++ Week1/Day4/ExerciseXP1.py
@@ -0,0 +1,29 @@
+
+
+# 🌟 Exercise 1: What Are You Learning?
+
+# Goal: Create a function that displays a message
+# about what you’re learning.
+
+# Key Python Topics:
+#    Functions (defining and calling)
+#    print() function
+
+# Step 1: Define a Function
+#    Define a function named display_message().
+#    This function should not take any parameters.
+
+def display_message():
+    print("I am learning about functions in Python.")
+
+display_message()
+
+# Step 2: Print a Message
+#    For example: “I am learning about functions in Python.”
+
+#Step 3: Call the Function
+#    This will execute the code inside the function
+#   and print your message.
+
+#Expected Output:
+#I am learning about functions in Python.
---
+++ Week1/Day4/ExerciseXP2.py
@@ -0,0 +1,27 @@
+
+# 🌟 Exercise 2: What’s Your Favorite Book?
+
+# Goal: Create a function that displays a message
+# about a favorite book.
+
+# Key Python Topics:
+#    Functions with parameters
+#    String concatenation or f-strings
+#    Calling functions with arguments
+
+# Step 1: Define a Function with a Parameter
+#    Define a function named favorite_book().
+#    This function should accept one parameter 
+#   called title.
+# Step 2: Print a Message with the Title
+#   The function needs to output a message like 
+#   “One of my favorite books is <title>”.
+# Step 3: Call the Function with an Argument
+#    Call the favorite_book() function and provide a 
+#   book title as an argument.
+#    For example: favorite_book("Alice in Wonderland").
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+favorite_book("Children of the King")
